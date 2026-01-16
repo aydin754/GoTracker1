9cbd81fd4179d16d77edcfda5605cdea9047e7f3
veerlol123 <vpatel8509@stu.d214.org>
2026-01-16 10:26:57 -0600

    completed firebase function

--- GoTracker1/TeamViewModel.swift
+++ GoTracker1/TeamViewModel.swift
@@ -14,7 +14,23 @@ import FirebaseFirestore
     func addTeam(nameOfTeam: String, nameOfPlayer: String, numberOfPlayer: String, positionOfPlayer: String ) {
         database.document(nameOfTeam).setData(["teamName": nameOfTeam,"player": nameOfPlayer, "playerNumber": numberOfPlayer, "playerPosition": positionOfPlayer ])
     }
-    
+    func pullFromDatabase() {
+        let database = Firestore.firestore().collection("teams")
+        database.addSnapshotListener { querySnapshot, error in
+            if let quertySnapshot = querySnapshot {
+                var downloadedTeams: [Team] = []
+                for document in quertySnapshot.documents {
+                    let nameOfTeam = document["teamName"] as! String
+                    let nameOfPlayer = document["player"] as! String
+                    let numberOfPlayer = document["playerNumber"] as! String
+                    let positionOfPlayer = document["playerPosition"] as! String
+                    
+                    let newTeam = Team(teamName: nameOfTeam, player: nameOfPlayer, playerNumber: numberOfPlayer, playerPosition: positionOfPlayer)
+                    downloadedTeams.append(newTeam)
+                }
+            }
+        }
+    }
     
     
 }
