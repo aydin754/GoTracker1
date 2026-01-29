f1aba3026dae16dff438b6c909517ee581606bec
veerlol123 <vpatel8509@stu.d214.org>
2026-01-29 10:24:57 -0600

    added map

--- GoTracker1/Field.swift
+++ GoTracker1/Field.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import MapKit
 struct Field: View {
     var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+        Map()
     }
 }
 
