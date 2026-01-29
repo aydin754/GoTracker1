22456d0b7a259bb8a894d7ae91a093d9f73d4b1a
veerlol123 <vpatel8509@stu.d214.org>
2026-01-29 11:04:22 -0600

    added code to zoom in the field and satelite the field

--- GoTracker1/Field.swift
+++ GoTracker1/Field.swift
@@ -8,8 +8,18 @@
 import SwiftUI
 import MapKit
 struct Field: View {
+    let fieldMap = CLLocationCoordinate2D(latitude: 42.105447, longitude: -87.956916)
+    @State var coordinates = MKCoordinateRegion( center: CLLocationCoordinate2D(latitude: 42.105447, longitude: -87.956916), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
     var body: some View {
-        Map()
+        Map(coordinateRegion: $coordinates)
+            .mapStyle(.imagery)
+            .onAppear() {
+                autoZoom(center: fieldMap)
+            }
+    }
+    func autoZoom(center: CLLocationCoordinate2D) {
+        let tightSpan = MKCoordinateSpan(latitudeDelta: 0.0010 , longitudeDelta: 0.0010)
+        coordinates = MKCoordinateRegion(center: center, span: tightSpan)
     }
 }
 
