beea00c6520d289b5b6ff72958b36f9f936fbec5
Aydin <Ametanovic8283@stu.d214.org>
2026-02-10 10:54:23 -0600

    I made a new file for BL where i made a class vars and the main func centeral manager

--- GoTracker1/SimpleBl.swift
+++ GoTracker1/SimpleBl.swift
@@ -6,4 +6,34 @@
 //
 import Foundation
 import CoreBluetooth
+ 
+
+class BluethoothSimple: NSObject, ObservableObject, CBCentralManagerDelegate {
+    
+    @Published var devices: [CBPeripheral] = []
+    @Published var status = "Scanning..."
+    
+    var manager: CBCentralManager!
+    
+    override init() {
+        super.init()
+        manager = CBCentralManager(delegate: self, queue: nil)
+    }
+}
+func centralMangerDidUpdateState(_ central: CBCentralManager) {
+    if central.state == .poweredOn {
+        status= "Scanning..."
+        manager.scanForPeripherals(withServices: nil)
+        {else}
+        status = "BlueTooth Off"
+    }
+}
+
+    
+    
+    
+    
+    
+    
+    
 
