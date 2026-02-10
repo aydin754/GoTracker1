2193b0fa3558fccd643bdb5fc7acbe31d4ddcc38
Aydin <Ametanovic8283@stu.d214.org>
2026-02-10 11:09:39 -0600

    Fixed errors

--- GoTracker1/SimpleBl.swift
+++ GoTracker1/SimpleBl.swift
@@ -8,33 +8,24 @@ import Foundation
 import CoreBluetooth
 import Combine
 
-
 class BluetoothSimple: NSObject, ObservableObject, CBCentralManagerDelegate {
-    
+
     @Published var devices: [CBPeripheral] = []
     @Published var status = "Scanning..."
-    
-    @State var manager: CBCentralManager!
-    
+
+    var manager: CBCentralManager!
+
     override init() {
         super.init()
         manager = CBCentralManager(delegate: self, queue: nil)
     }
-}
-func centralMangerDidUpdateState(_ central: CBCentralManager) {
-    if central.state == .poweredOn {
-        status = "Scanning..."
-        manager.scanForPeripherals(withServices: nil)
-        {else}
-        status = "BlueTooth Off"
+
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            status = "Scanning..."
+            manager.scanForPeripherals(withServices: nil)
+        } else {
+            status = "Bluetooth Off"
+        }
     }
 }
-
-    
-    
-    
-    
-    
-    
-    
-
