83e74e35d47e33bae2e5e8b0655fba400c740aae
Aydin <Ametanovic8283@stu.d214.org>
2026-02-25 11:09:04 -0600

    added a nice background collor

--- GoTracker1/ContentView.swift
+++ GoTracker1/ContentView.swift
@@ -9,60 +9,82 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image("school")
-            .resizable()
-            .scaledToFit()
-            .frame(width: 250)
-        
-        
-        
-        
         NavigationStack{
-        
+            
+            
+            
+            ZStack{
+                // this code is for the custom color on the homepage black with orange
+                LinearGradient(
+                    colors: [
+                        Color.black,
+                        Color.orange.opacity(0.9),
+                        Color.black
+                    ],
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                )
+                .ignoresSafeArea()
+                
+                
+                
+                
+                
+                
+                
+                
+                
                 VStack{
                     
-                
-            
-                
+                    Image("school")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 250)
+                        .opacity(0.9)
                     
                     
-                
-                Text("Hersey Go Tracker")
-                        .foregroundStyle(.brown)
-                    .position(x: 180, y: 10)
-                
-                NavigationLink("Administrator"){
-                    Admin()
-                        .position(x:180, y: 40)
+                    
+                    
+                    
+                    Text("Hersey Go Tracker")
+                        .font(.largeTitle)
+                    
+                        .foregroundColor(.white)
+                        .position(x: 180, y: 10)
+                    
+                    NavigationLink("Administrator"){
+                        Admin()
+                            .position(x:180, y: 40)
+                        
+                    }
+                    .font(.title)
+                    .foregroundColor(.orange)
+                    
+                    NavigationLink("Guest page"){
+                        Guest()
+                    }
+                    .font(.title)
+                    .foregroundColor(.orange)
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    .bold()
+                    .padding()
+                    
                     
                 }
-                .font(.title)
-                .foregroundColor(.orange)
-                    
-                NavigationLink("Guest page"){
-                    Guest()
-                }
-                .font(.title)
-                .foregroundColor(.orange)
+                
+                
+            }
             
-                    
-                    
-                    
-                
-                
-                
-
-                
-                
         }
-       
-           
-        }
-        .bold()
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
+
